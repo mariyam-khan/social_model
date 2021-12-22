a6a663f5d3c78d462544fc7ab92135e7d616effc
tilman <tilmandiego.aleman@stud.uni-goettingen.de>
2021-12-22 18:42:25 +0100

    refactoring, implemented bitwise majority

--- src/government.py
+++ src/government.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 # An abstract government defining the Interface. 
 class AbstractGovernment():
     def __init__(self):
@@ -15,17 +15,28 @@ class AbstractGovernment():
 # A member of our population. might be extended with updating rules etc
 class Pop():
     def __init__(self, Ocenter, I, bias, candidate=False):
-        self.Ocenter = O
+        self.Ocenter = Ocenter
         self.I = I
         self.bias = bias
         self.candidate = candidate
+        self.optimum = Ocenter
+        self.policysize = len(Ocenter)
+
 
     # find the local optimizer given the utility. should debate if utility should be fixed in constructor or here, but not too important I guess
     def findOptimum(self, utility):
         raise NotImplementedError()
 
     def __str__(self):
-        raise NotImplementedError()
+        ret = "Center: " + str(self.Ocenter) + "\n"
+        ret += "Radius: " + str(self.I) + "\n"
+        ret += "Bias: " + str(self.bias) + "\n"
+        if self.candidate:
+            ret += "Is a Candidate" + "\n"
+        ret += "Optimal Opinion: " + str(self.optimum) + "\n"
+        ret += "Size of Policy: " + str(self.policysize) + "\n"
+        
+        return ret
 
 # A general population class. We might implement different realizations of populations or use samples from different populations in a single simulation
 class AbstractPopulation():
@@ -38,11 +49,30 @@ class AbstractPopulation():
     
     def __str__(self):
         raise NotImplementedError()
-
+class UniformPopulation(AbstractPopulation):
+    def __init__(self):
+        pass
+    def sample(self, samplesize, policysize):
+        I=0
+        bias=0
+        return [Pop(np.random.randint(0,2,policysize), I, bias) for k in range(samplesize)]
 
 # Needs Population and Pop classes to have a working prototype
 class BitMajDirectDemocracy(AbstractGovernment):
     def __init__(self):
         pass
+    def computePolicy(self, pops):
+        n = len(pops)
+        d = pops[0].policysize
+        result = np.zeros(d, dtype=int)
+        for pop in pops:
+            result += pop.optimum
+        result = (result > n//2)
+        return result 
 
-    
+a = UniformPopulation()
+pops = a.sample(3, 1)
+b = BitMajDirectDemocracy()
+print(b.computePolicy(pops))
+for pop in pops:
+    print(pop)    
